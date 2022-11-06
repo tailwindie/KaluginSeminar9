24c3faaf30d5938ef72cd5cb92d389c50b3e3ebc
Mikhail <jeveuxmourir31613@gmail.com>
2022-11-06 17:35:23 +0300

    seminar3 hw

--- Program.cs
+++ Program.cs
@@ -1,68 +1,97 @@
 ﻿/*
-// Задача №10. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// Задача №19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
-Console.Write("Введите трёхзначное число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-double tail1 = a / 100;
-double tail2 = a % 10;
-
-int i = 0;
-while (i < tail1)
+void Palindrome(int num)
 {
-    a -= 100;
-    i++;
+  int num1 = num;
+  int num2 = 0;
+
+while (num > 0)
+{
+    num2 = (num % 10) + (num2 * 10 );
+    num /= 10;
 }
 
-int j = 0;
-while (j < tail2)
+if (num2 != num1)
 {
-    a--;
-    j++;
-}
-
-a /= 10;
-
-Console.Write($"Вторая цифра данного числа: {a}");
-*/
-//----------------------------------------------------------
-/*
-// Задача №13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-
-Console.Write("Введите число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-
-double tail = a / 100;
-
-if (tail <= 0)
-{
-    Console.Write("У вашего числа нет третьей цифры!");
+  Console.Write($"{num1} - не палиндром! Вот оно наоборот: {num2}.");  
 }
 else
 {
-    tail = tail % 10;
-    Console.Write($"Третья цифра вашего числа: {tail}");
+  Console.Write($"{num1} - это палиндром!. Вот оно наоборот: {num2}");
+}
+};
+
+while (true)
+{
+  Console.Write("Введите пятизначное число: ");
+  int num = Convert.ToInt32(Console.ReadLine() );
+
+  if (num>=10000 && num<=99999)
+  {
+    Palindrome(num);
+    break;
+  }
+  else
+  {
+    Console.Write("Вы ввели не пятизначное число!") ;
+  };
 }
 */
 //----------------------------------------------------------
 /*
-//Задача №15. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// Задача №21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
-Console.Write("Введите число: ");
-int a = Convert.ToInt32(Console.ReadLine());
+double Coords(int xa, int ya, int za, int xb, int yb, int zb)
+{
+ double abz = Math.Sqrt( ((xb-xa) * (xb-xa)) + ((yb-ya) * (yb-ya)) + ((zb-za) * (zb-za)) );
+ return abz;
+}
 
-if (a > 7 || a < 1)
+Console.Write("Введите X координату точки A: ");
+int xa1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Y координату точки A: ");
+int ya1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Z координату точки A: ");
+int za1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите X координату точки B: ");
+int xb1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Y координату точки B: ");
+int yb1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Z координату точки B: ");
+int zb1 = Convert.ToInt32(Console.ReadLine());
+
+
+
+double distance = Coords(xa1, ya1, za1, xb1, yb1, zb1);
+
+Console.Write($"Расстояние между точками A ({xa1}, {ya1}, {za1}) и B ({xb1}, {yb1}, {zb1}) = {distance} ");
+*/
+//----------------------------------------------------------
+/*
+// Задача №23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void Cube(int N)
 {
-    Console.Write("В неделе всего 7 дней! Введите число от 1 до 7.");
-}
-else
-{
-    if (a < 6)
+    if (N != 0)
     {
-        Console.Write("Эх, будний день! Придётся на работу.");
+        int a = 0;
+        int i = 1;
+        while (i <= N)
+        {
+            a = i*i*i;
+            Console.Write($"{a} ");
+            i++;
+        } 
     }
-    else if (a >= 6)
+    else
     {
-        Console.Write("Опа, выходной! Я за пивом, с тебя чипсы!");
+        Console.Write("Умный ход! Вот ваша таблица: 0");
     }
-}
+};
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Cube(N);
 */
