dfe5cfd766f37492866132e9170d804939897818
Mikhail <jeveuxmourir31613@gmail.com>
2022-11-11 12:56:28 +0300

    seminar5 hw

--- Program.cs
+++ Program.cs
@@ -1,93 +1,114 @@
 ﻿/*
-// Задача №25. Напишите программу, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B (Math.Pow НЕ использовать)
-
-int Powww(int a, int b)
-{
-  int i = 1 ;
-  int res = 1 ;
-
-  while( i <= b )
-  {
-    if( b==0 )
-    {
-      res  = 1;
-      break; 
-    }
-    else
-    {
-      res *= a;
-      i++ ;
-    }
-  }
-  return res;
-}
-
-Console.WriteLine("Введите число: ");
-int a1 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Укажите степень: ");
-int b1 = Convert.ToInt32(Console.ReadLine());
-
-int result = Powww(a1,b1);
-
-Console.WriteLine($"{a1} в {b1} степени = {result}");
-*/
-//----------------------------------------------------------
-/*
-// Задача №27. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-
-int Count(int a)
-{
-  int res = 0;
-  while(a > 0)
-  {
-    res += (a % 10);
-    a /= 10;
-  }
-  return res ;
-}
-
-  Console.Write("Введите число: ");
-  int a1 = Convert.ToInt32(Console.ReadLine());
-
-
-  if(a1 <=0)
-  {
-    Console.WriteLine($"Сумма цифр в вашем числе = 0.");
-    Console.WriteLine($"Возможно, потому что вы просто написали 0 или отрицательное число.");
-    Console.WriteLine($"Попробуйте заново. Впишите число больше нуля.");
-  }
-  else
-  {
-    int result = Count(a1);
-    Console.WriteLine($"Сумма цифр в числе {a1} = {result}");
-  }
-*/
-//----------------------------------------------------------
-/*
-// Задача №29. Напишите программу, которая создает массив заданного пользователем размера, заполняет массив элементами от 1 до 99 и выводит их на экран.
+// Задача №34. Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 
 int [] GetArray(int n)
 {
   int [] arr = new int[n];
   for (int i = 0; i < n; i++)
   {
-    arr[i] = new Random().Next(1 , 100);
+    arr[i] = new Random().Next(100 , 1000);
   }
   return arr;
 }
 
-void ShowArray(int [] arr)
+void ShowCount(int [] arr)
 {
+  int res = 0;
   Console.Write($"Ваш массив: ");
   for( int i = 0; i < arr.Length; i++)
   {
     Console.Write($"{arr[i]} ");
+
+    if (arr[i] % 2 == 0)
+    {
+      res += 1;
+    }
   }
+  Console.Write($". Количество чётных чисел = {res}.");
 }
  
 Console.Write("Введите размер массива: ");
 int n1 = Convert.ToInt32(Console.ReadLine());
 
-ShowArray(GetArray(n1));
+ShowCount(GetArray(n1));
+*/
+//----------------------------------------------------------
+
+// Задача №36. Задайте массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+/*
+int [] GetArray(int n)
+{
+  int [] arr = new int[n];
+  for (int i = 0; i < n; i++)
+  {
+    arr[i] = new Random().Next();
+  }
+  return arr;
+}
+
+void ShowCount(int [] arr)
+{
+  int res = 0;
+  Console.Write($"Ваш массив: ");
+  for( int i = 0; i < arr.Length; i++)
+  {
+    Console.Write($"{arr[i]} ");
+
+    if (i % 2 == 0)
+    {
+      res += arr[i];
+    }
+  }
+  Console.Write($". Сумма чисел на нечётных позициях = {res}.");
+}
+ 
+Console.Write("Введите размер массива: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+ShowCount(GetArray(n1));
+*/
+//----------------------------------------------------------
+/*
+// Задача №38. Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+double [] GetArray(int n)
+{
+  double [] arr = new double[n];
+  for (int i = 0; i < n; i++)
+  {
+    arr[i] = Math.Round(new Random().NextDouble(), 1);
+    arr[i] += new Random().Next(0, 100);
+  }
+  return arr;
+}
+
+void ShowCount(double [] arr)
+{
+  double maxArr = arr[0];
+  double minArr = arr[0];
+  
+  Console.Write($"Ваш массив: ");
+  for( int i = 0; i < arr.Length; i++)
+  {
+    Console.Write($"{arr[i]} ");
+  }
+  for( int j = 1; j < arr.Length; j++)
+  {
+    if (arr[j] > arr[j-1])
+    {
+      maxArr = arr[j];
+    }
+    else if (arr[j] < arr[j-1])
+    {
+      minArr = arr[j];
+    }
+  }
+  double res = Math.Round(maxArr - minArr, 1);
+  Console.Write($". Разница между максимальным и минимальным элементами массива = {maxArr} - {minArr} = {res}.");
+}
+
+Console.Write("Введите размер массива: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+ShowCount(GetArray(n1));
 */
