daeafef907d623713910aa487a8a84a591bbef7d
Mikhail <jeveuxmourir31613@gmail.com>
2022-11-27 17:02:48 +0300

    seminar8 hw

--- Program.cs
+++ Program.cs
@@ -1,90 +1,10 @@
 ﻿/*
-// Задача №47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// Задача №54. Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
 
-double [,] GetArray(int m, int n)
-{
-  double [,] arr = new double[m, n];
-  Console.WriteLine($"Ваш массив: ");
-  for (int i = 0; i < m; i++)
-  {
-    Console.Write($"\n");
-    for (int j = 0; j < n; j++)
-    {
-      arr[i, j] = Math.Round(new Random().NextDouble(), 1);
-      arr[i, j] += new Random().Next(0, 100);
-      Console.Write($"{arr[i, j]}   ");
-    }
-  }
-  return arr;
-}
- 
-Console.Write("Введите m: ");
-int m1 = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Введите n: ");
-int n1 = Convert.ToInt32(Console.ReadLine());
-
-GetArray(m1, n1);
-*/
-//----------------------------------------------------------
-/*
-// Задача №50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
-
-double [,] GetArray(int n, int m)
-{
-  double [,] arr = new double[n, m];
-  Console.WriteLine($"Ваш массив: ");
-  for (int i = 0; i < n; i++)
-  {
-    Console.Write($"\n");
-    for (int j = 0; j < m; j++)
-    {
-      arr[i, j] = Math.Round(new Random().NextDouble(), 1);
-      arr[i, j] += new Random().Next(0, 100);
-      Console.Write($"{arr[i, j]}   ");
-    }
-  }
-  return arr;
-}
-
-void Checkout (double [,] arr)
-{
-  Console.Write($"\n");
-  Console.Write($"\n");
-  Console.Write("Введите позицию элемента m: ");
-  int m2 = Convert.ToInt32(Console.ReadLine());
-
-  Console.Write("Введите позицию элемента n: ");
-  int n2 = Convert.ToInt32(Console.ReadLine());
-
-  int rows = arr.GetUpperBound(0) + 1;
-  int cols = arr.Length / rows;
-  if (m2 > rows || n2 > cols)
-  {
-    Console.Write($"Такого элемента нет!");
-  }
-  else
-  {
-    Console.Write($"Ваш элемент: {arr[m2-1, n2-1]}");
-  }
-}
-
- 
-Console.Write("Введите m: ");
-int m1 = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Введите n: ");
-int n1 = Convert.ToInt32(Console.ReadLine());
-
-Checkout(GetArray(m1, n1));
-*/
-//----------------------------------------------------------
-/*
-// Задача №52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 int [,] TooManyArrays(int m, int n)
 {
   int [,] arr = new int[n, m];
-  Console.WriteLine($"Ваш массив: ");
+  Console.WriteLine($"Введённый массив: ");
 
   for (int i = 0; i < n; i++)
   {
@@ -98,29 +18,39 @@ int [,] TooManyArrays(int m, int n)
   return arr;
 }
 
-void Checkout (int [,] arr)
+void Sorting (int [,] arr)
 {
   Console.Write($"\n");
   Console.Write($"\n");
   int rows = arr.GetUpperBound(0) + 1;
   int cols = arr.Length / rows;
 
-  double []resArr = new double[cols];
-
-  for (int g = 0; g < cols; g++)
+  for (int g = 0; g < rows; g++)
   {
-    
-    for (int p = 0; p < rows; p++)
+
+    for (int c = 0; c < cols; c++)
     {
-      resArr[g] += arr[p, g];
-      
+
+      for (int p = 0; p < cols-1; p++)
+      {
+
+        if (arr[g, p] < arr[g,p+1])
+        {
+          int temp = arr[g, p];
+          arr[g, p] = arr[g, p+1];
+          arr[g, p+1] = temp;
+        }
+      }
     }
   }
-  for (int f = 0; f < cols; f++)
+  Console.WriteLine($"Изменённый массив: ");
+  for (int f = 0; f < rows; f++)
   {
-      resArr[f] /= rows;
-      resArr[f] = Math.Round(resArr[f], 1);
-      Console.Write($"{resArr[f]}   ");
+    Console.Write($"\n");
+    for (int h = 0; h < cols; h++)
+    {
+      Console.Write($"{arr[f, h]}   ");
+    }
   }
 }
  
@@ -130,5 +60,145 @@ int m1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите n: ");
 int n1 = Convert.ToInt32(Console.ReadLine());
 
-Checkout(TooManyArrays(m1, n1));
+Sorting(TooManyArrays(m1, n1));
+*/
+//----------------------------------------------------------
+/*
+// Задача №56. Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+int [,] TooManyArrays(int m, int n)
+{
+  int [,] arr = new int[n, m];
+  Console.WriteLine($"Введённый массив: ");
+
+  for (int i = 0; i < n; i++)
+  {
+    Console.Write($"\n");
+    for (int j = 0; j < m; j++)
+    {
+      arr[i, j] = new Random().Next(0, 10);
+      Console.Write($"{arr[i, j]}   ");
+    }
+  }
+  Console.Write($"\n");
+  Console.Write($"\n");
+  return arr;
+}
+
+int[] SomeRows(int[,] arr) 
+{
+  int[] calcArr = new int[arr.GetLength(0)];
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    int sum = 0;
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      sum += arr[i, j];
+    }
+    Console.WriteLine($"Сумма элементов в {i + 1} строчке: {sum} ");
+    calcArr[i] = sum;
+  }
+  return calcArr;
+}
+
+Console.Write("Введите m: ");
+int m1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите n: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+int[,] someArr = TooManyArrays(m1, n1);
+
+int[] sumArr = SomeRows(someArr);
+int min = sumArr[0];
+
+int min1 = 0;
+for (int i = 1; i < sumArr.Length; i++)
+{
+    if (sumArr[i] < min)
+    {
+        min = sumArr[i];
+        min1 = i;
+    }
+}
+Console.Write($"Наименьшая сумма элементов в {min1 + 1} строке.");
+*/
+//----------------------------------------------------------
+/*
+// Задача №62. Напишите программу, которая заполнит спирально массив 4 на 4.
+
+int rows = 4;
+int cols = 4;
+
+void TooManyArrays(int[,] arr)
+{
+  for (int i = 0; i < arr.GetLength(0); i++) 
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+
+        Console.Write($"{arr[i, j], 3} ");
+
+    }
+    Console.WriteLine();
+  }
+}
+int SpiralArr(int[,] newArr, int count, int iE, int jE) 
+{
+ 
+    for (int j = jE; j < cols; j++)
+    {
+        if (newArr[iE, j] == 0) 
+        {
+          newArr[iE, j] = count;
+        }
+        count++;
+    }
+    
+    for (int i = iE + 1; i < rows; i++)
+    {
+        if (newArr[i, cols - 1] == 0)
+        {
+          newArr[i, cols - 1] = count;
+        }
+        count++;
+    }
+ 
+    for (int j = cols - 2; j >= jE; j--) 
+    {
+        if (newArr[rows - 1, j] == 0)
+        {
+          newArr[rows - 1, j] = count;
+        }
+        count++;
+    }
+ 
+    for (int i = rows - 2; i > iE; i--)
+    {
+        if (newArr[i, jE] == 0)
+        {
+          newArr[i, jE] = count;
+        }
+        count++;
+    }
+   
+    return count;
+}
+
+int[,] someSpiralArray = new int[rows, cols];
+int i1 = 0;
+ int j1 = 0;
+int value = 1;
+
+while (rows > 1 & cols > 1)
+{
+  value = SpiralArr(someSpiralArray, value, i1, j1);
+  i1++;
+  j1++;
+  rows--;
+  cols--;
+   
+}
+ 
+TooManyArrays(someSpiralArray);
 */
