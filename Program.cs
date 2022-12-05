60697135b37ad1dee5a08a3e5b2bad3c235cde10
Mikhail <jeveuxmourir31613@gmail.com>
2022-12-05 13:45:47 +0300

    seminar9 hw

--- Program.cs
+++ Program.cs
@@ -1,204 +1,64 @@
 ﻿/*
-// Задача №54. Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Задача №64. Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
 
-int [,] TooManyArrays(int m, int n)
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string NaturalNumbers(int m1, int n1)
 {
-  int [,] arr = new int[n, m];
-  Console.WriteLine($"Введённый массив: ");
-
-  for (int i = 0; i < n; i++)
-  {
-    Console.Write($"\n");
-    for (int j = 0; j < m; j++)
-    {
-      arr[i, j] = new Random().Next(0, 10);
-      Console.Write($"{arr[i, j]}   ");
-    }
-  }
-  return arr;
+    if (m1 <= n1) return $"{m1}   " + NaturalNumbers(m1 + 1, n1);
+    else return String.Empty;
 }
-
-void Sorting (int [,] arr)
-{
-  Console.Write($"\n");
-  Console.Write($"\n");
-  int rows = arr.GetUpperBound(0) + 1;
-  int cols = arr.Length / rows;
-
-  for (int g = 0; g < rows; g++)
-  {
-
-    for (int c = 0; c < cols; c++)
-    {
-
-      for (int p = 0; p < cols-1; p++)
-      {
-
-        if (arr[g, p] < arr[g,p+1])
-        {
-          int temp = arr[g, p];
-          arr[g, p] = arr[g, p+1];
-          arr[g, p+1] = temp;
-        }
-      }
-    }
-  }
-  Console.WriteLine($"Изменённый массив: ");
-  for (int f = 0; f < rows; f++)
-  {
-    Console.Write($"\n");
-    for (int h = 0; h < cols; h++)
-    {
-      Console.Write($"{arr[f, h]}   ");
-    }
-  }
-}
- 
-Console.Write("Введите m: ");
-int m1 = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Введите n: ");
-int n1 = Convert.ToInt32(Console.ReadLine());
-
-Sorting(TooManyArrays(m1, n1));
+Console.WriteLine(NaturalNumbers(m, n));
 */
 //----------------------------------------------------------
 /*
-// Задача №56. Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Задача №66. Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 
-int [,] TooManyArrays(int m, int n)
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int NaturNumsSum(int m1, int n1)
 {
-  int [,] arr = new int[n, m];
-  Console.WriteLine($"Введённый массив: ");
-
-  for (int i = 0; i < n; i++)
-  {
-    Console.Write($"\n");
-    for (int j = 0; j < m; j++)
-    {
-      arr[i, j] = new Random().Next(0, 10);
-      Console.Write($"{arr[i, j]}   ");
-    }
-  }
-  Console.Write($"\n");
-  Console.Write($"\n");
-  return arr;
+    if (m1 != n1 + 1) return m1 + NaturNumsSum(m1 + 1, n1) ; 
+    else return 0;
 }
-
-int[] SomeRows(int[,] arr) 
-{
-  int[] calcArr = new int[arr.GetLength(0)];
-  for (int i = 0; i < arr.GetLength(0); i++)
-  {
-    int sum = 0;
-    for (int j = 0; j < arr.GetLength(1); j++)
-    {
-      sum += arr[i, j];
-    }
-    Console.WriteLine($"Сумма элементов в {i + 1} строчке: {sum} ");
-    calcArr[i] = sum;
-  }
-  return calcArr;
-}
-
-Console.Write("Введите m: ");
-int m1 = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Введите n: ");
-int n1 = Convert.ToInt32(Console.ReadLine());
-
-int[,] someArr = TooManyArrays(m1, n1);
-
-int[] sumArr = SomeRows(someArr);
-int min = sumArr[0];
-
-int min1 = 0;
-for (int i = 1; i < sumArr.Length; i++)
-{
-    if (sumArr[i] < min)
-    {
-        min = sumArr[i];
-        min1 = i;
-    }
-}
-Console.Write($"Наименьшая сумма элементов в {min1 + 1} строке.");
+Console.WriteLine($"Cумма всех натуральных чисел в промежутке от {m} до {n} = {NaturNumsSum(m, n)} ");
 */
 //----------------------------------------------------------
 /*
-// Задача №62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Задача №68.  Напишите программу вычисления функции Аккермана с помощью рекурсии.
 
-int rows = 4;
-int cols = 4;
-
-void TooManyArrays(int[,] arr)
+long Akkerman (long m, long n) 
 {
-  for (int i = 0; i < arr.GetLength(0); i++) 
-  {
-    for (int j = 0; j < arr.GetLength(1); j++)
-    {
+  if (m == 0) return n + 1;
 
-        Console.Write($"{arr[i, j], 3} ");
+  if (m > 0 && n == 0) return Akkerman(m - 1, 1);
 
-    }
-    Console.WriteLine();
-  }
-}
-int SpiralArr(int[,] newArr, int count, int iE, int jE) 
-{
- 
-    for (int j = jE; j < cols; j++)
-    {
-        if (newArr[iE, j] == 0) 
-        {
-          newArr[iE, j] = count;
-        }
-        count++;
-    }
-    
-    for (int i = iE + 1; i < rows; i++)
-    {
-        if (newArr[i, cols - 1] == 0)
-        {
-          newArr[i, cols - 1] = count;
-        }
-        count++;
-    }
- 
-    for (int j = cols - 2; j >= jE; j--) 
-    {
-        if (newArr[rows - 1, j] == 0)
-        {
-          newArr[rows - 1, j] = count;
-        }
-        count++;
-    }
- 
-    for (int i = rows - 2; i > iE; i--)
-    {
-        if (newArr[i, jE] == 0)
-        {
-          newArr[i, jE] = count;
-        }
-        count++;
-    }
-   
-    return count;
+  if (m > 0 && n > 0) return Akkerman(m - 1, Akkerman(m, n - 1));
+
+  return Akkerman(m,n);
 }
 
-int[,] someSpiralArray = new int[rows, cols];
-int i1 = 0;
- int j1 = 0;
-int value = 1;
 
-while (rows > 1 & cols > 1)
+Console.Write($"Введите число M: ");
+long m = (long)Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Введите число N: ");
+long n = (long)Convert.ToInt32(Console.ReadLine());
+
+if (m >= 0 && n >= 0)
 {
-  value = SpiralArr(someSpiralArray, value, i1, j1);
-  i1++;
-  j1++;
-  rows--;
-  cols--;
-   
+  Console.WriteLine($"Функция Аккермана с числами {m} и {n} = {Akkerman(m,n)}");
 }
- 
-TooManyArrays(someSpiralArray);
+else
+{
+  Console.Write($"Какое-то из чисел меньше или равно 0. Введите, пожалуйста, два неотрицательных числа.");
+}
 */
